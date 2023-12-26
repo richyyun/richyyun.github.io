519b27e4d4a4724ff577d379bd4e92cf41842511
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-26 14:45:32 -0800

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -10,7 +10,7 @@ function activate(){
 
     var nav = document.getElementsByClassName(".topnav a");
 
-    for (i; i<nav.length; i++){
+    for (let i=0; i<nav.length; i++){
         var name = nav[i].href.split("/");
         name = name[name.length - 1];
         nav[i].addClass("active");
