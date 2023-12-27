3782b840a0988c3810525bdf1b2976522fddaa44
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-26 19:07:40 -0800

    Testing js

--- navbar.js
+++ navbar.js
@@ -1,14 +1,17 @@
 /*
 Functions for interactive navigation bar
 */
+var nav = document.getElements("a");
+nav[0].classList.add("active")
 
+/*
 function activate(){
     
     // Get URL 
     var url = window.location.href.split("/");
     var currentPage = url[url.length - 1];
 
-    var nav = document.getElementById('topnav').children;
+    var nav = document.getElements(".topnav a");
 
     for (let i=0; i<nav.length; i++){
         var name = nav[i].href.split("/");
@@ -22,3 +25,4 @@ function activate(){
 
 
 }
+*/
