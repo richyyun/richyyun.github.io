441b96abac9731a6eaf637f6a7c635fb6650ebdb
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-28 23:55:38 -0800

    Update about.js

--- js/about.js
+++ js/about.js
@@ -7,42 +7,60 @@ var sonera = document.getElementById("sonera");
 var fetz = document.getElementById("fetz");
 var nicolelis = document.getElementById("nicolelis");
 
+/* To correct for mouse moving out too fast not being captured */
+var soneraMouse = false;
+var fetzMouse = false;
+var nicolelisMouse = false;
 
 function addFade(elem, flag){
     elem.animate(
         {opacity:[1,0]}, 
         {duration:250, fill:'none'}
         ).addEventListener("finish",function(){
-            {elem.style.opacity=0}
+            if (flag){elem.style.opacity=0}
+            else {elem.style.opacity=1}            
         });
 }
 
 sonera.addEventListener('mouseover', ()=> {
+    soneraMouse = true;
     addFade(fetz, soneraMouse);
     addFade(nicolelis, soneraMouse);
 });
 
 sonera.addEventListener('mouseout', ()=> {
+    soneraMouse = false;
     fetz.style.opacity = 1;
     nicolelis.style.opacity = 1;
 });
 
+sonera.addEventListener('mousemove', ()=> {
+    if (!soneraMouse){
+        fetz.style.opacity = 1;
+        nicolelis.style.opacity = 1;
+    }
+});
+
 fetz.addEventListener('mouseover', ()=> {
+    fetzMouse = true;
     addFade(sonera, fetzMouse);
     addFade(nicolelis, fetzMouse);
 });
 
 fetz.addEventListener('mouseout', ()=> {
+    fetzMouse = false;
     sonera.style.opacity = 1;
     nicolelis.style.opacity = 1;
 });
 
 nicolelis.addEventListener('mouseover', ()=> {
+    nicolelisMouse = true;
     addFade(sonera, nicolelisMouse);
     addFade(fetz, nicolelisMouse);
 });
 
 nicolelis.addEventListener('mouseout', ()=> {
+    nicolelisMouse = false;
     sonera.style.opacity = 1;
     fetz.style.opacity = 1;
 });
