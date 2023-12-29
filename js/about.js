a3ce80b074970b23f8e8543c70380e33480dbb8e
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-28 23:44:35 -0800

    Update about.js

--- js/about.js
+++ js/about.js
@@ -2,14 +2,21 @@ var sonera = document.getElementById("sonera");
 var fetz = document.getElementById("fetz");
 var nicolelis = document.getElementById("nicolelis");
 
+var soneraMouse = false;
+
 sonera.addEventListener('mouseover', ()=> {
+    soneraMouse = true;
     fetz.animate(
         {opacity:[1,0]}, 
         {duration:250, fill:'none'}
-        ).addEventListener("finish",function(){fetz.style.opacity=0});
+        ).addEventListener("finish",function(){
+            if (soneraMouse){fetz.style.opacity=0}
+            else {fetz.style.opacity=1}            
+        });
 });
 
 sonera.addEventListener('mouseout', ()=> {
+    soneraMouse = false;
     fetz.style.opacity = 1;
     nicolelis.style.opacity = 1;
 });
