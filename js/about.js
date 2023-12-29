9e4e03b80a69ab06bfca01b17b69787664a015ca
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-28 21:45:26 -0800

    javascript test

---
+++ js/about.js
@@ -0,0 +1,8 @@
+var sonera = document.getElementById("sonera");
+var fetz = document.getElementById("fetz");
+var nicolelis = document.getElementById("nicolelis");
+
+sonera.addEventListener('mouseover', ()=> {
+    fetz.style.opacity = 0;
+    nicolelis.style.opacity = 0;
+});
