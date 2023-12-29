03b8b671a4d2b1409cc78ebac591774df19623f7
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-29 00:51:38 -0800

    jquery test

--- js/about.js
+++ js/about.js
@@ -1,14 +1,27 @@
-/* Animations for the bottom three images 
-Turns out CSS isn't enough for mouseover affecting various other elements.
-This script allows for inter-dependent animation effects.
-*/
+/* Animations for the bottom three images */
 
+$("#sonera").on("mouseover", function(){
+    $("#fetz").fadeTo(1, 0);
+});
+$("#sonera").on("mouseout", function(){
+    $("#fetz").css("opacity", 1);
+});
+
+
+/* Javascript works 90% of the way but cannot seem to figure out a 
+way to prevent fast mouse movements from affecting the animation.
+Setting fill forwards locks you out of setting the opacity again
+even with a parent wrapper. Adding an event listener so it sets the 
+opacity to 0 when the animation finishes means if the mouse were 
+to leave before the animation finished it would still get set to 0*/
+
+
+/*
 var sonera = document.getElementById("sonera");
 var fetz = document.getElementById("fetz");
-var fetzparent = document.getElementById("fetzparent");
 var nicolelis = document.getElementById("nicolelis");
 
-/* To correct for mouse moving out too fast not being captured */
+// To correct for mouse moving out too fast not being captured 
 var soneraMouse = false;
 var fetzMouse = false;
 var nicolelisMouse = false;
@@ -17,9 +30,9 @@ function addFade(elem, flag){
     elem.animate(
         {opacity:[1,0]}, 
         {duration:100, fill:'forwards'}
-        );/*.addEventListener("finish",function(){
-            {elem.style.opacity=0}
-        });*/
+        );//.addEventListener("finish",function(){
+        //    {elem.style.opacity=0}
+        //});
 }
 
 sonera.addEventListener('mouseenter', ()=> {
@@ -64,4 +77,4 @@ nicolelis.addEventListener('mouseout', ()=> {
     sonera.style.opacity = 1;
     fetz.style.opacity = 1;
 });
-
+*/
