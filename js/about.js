09e4a8401e389085e972a83be89e6235abb42961
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-28 23:52:34 -0800

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,27 +1,49 @@
+/* Animations for the bottom three images 
+Turns out CSS isn't enough for mouseover affecting various other elements.
+This script allows for inter-dependent animation effects.
+*/
+
 var sonera = document.getElementById("sonera");
 var fetz = document.getElementById("fetz");
 var nicolelis = document.getElementById("nicolelis");
 
-/* To correct for mouse moving out too fast not being captured */
-var soneraMouse = false;
-
-sonera.addEventListener('mouseover', ()=> {
-    soneraMouse = true;
-    addFade(fetz, soneraMouse);
-});
-
-sonera.addEventListener('mouseout', ()=> {
-    soneraMouse = false;
-    fetz.style.opacity = 1;
-    nicolelis.style.opacity = 1;
-});
 
 function addFade(elem, flag){
     elem.animate(
         {opacity:[1,0]}, 
         {duration:250, fill:'none'}
         ).addEventListener("finish",function(){
-            if (flag){elem.style.opacity=0}
-            else {elem.style.opacity=1}            
+            {elem.style.opacity=0}
         });
 }
+
+sonera.addEventListener('mouseover', ()=> {
+    addFade(fetz, soneraMouse);
+    addFade(nicolelis, soneraMouse);
+});
+
+sonera.addEventListener('mouseout', ()=> {
+    fetz.style.opacity = 1;
+    nicolelis.style.opacity = 1;
+});
+
+fetz.addEventListener('mouseover', ()=> {
+    addFade(sonera, fetzMouse);
+    addFade(nicolelis, fetzMouse);
+});
+
+fetz.addEventListener('mouseout', ()=> {
+    sonera.style.opacity = 1;
+    nicolelis.style.opacity = 1;
+});
+
+nicolelis.addEventListener('mouseover', ()=> {
+    addFade(sonera, nicolelisMouse);
+    addFade(fetz, nicolelisMouse);
+});
+
+nicolelis.addEventListener('mouseout', ()=> {
+    sonera.style.opacity = 1;
+    fetz.style.opacity = 1;
+});
+
