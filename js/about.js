b9503e21f77f1d634f818b3ba26fff8f675ccb95
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-28 22:13:37 -0800

    Update about.js

--- js/about.js
+++ js/about.js
@@ -6,3 +6,8 @@ sonera.addEventListener('mouseover', ()=> {
     fetz.style.opacity = 0;
     nicolelis.style.opacity = 0;
 });
+
+sonera.addEventListener('mouseout', ()=> {
+    fetz.style.opacity = 1;
+    nicolelis.style.opacity = 1;
+});
