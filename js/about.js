2519cf9b8142946e313008b63cce4131f0b0a3f3
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-29 18:55:49 -0800

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,11 +1,15 @@
-/* Animations for the bottom three images */
+/* Animations for the bottom three images 
+TODO: make into functions so it's not so much copy paste. 
+*/
 var fadedur = 100;
 var fadeopacity = 0.2;
+var scaledur = 100;
+var scalefactor = 1.1;
 
 $("#sonera").hover(function () {
         $("#sonera").animate({
-            scale: 1.2
-        }, 250 );
+            scale: scalefactor
+        }, scaledur );
         $("#fetz").fadeTo(fadedur, fadeopacity);
         $("#nicolelis").fadeTo(fadedur, fadeopacity);
     }, function () {
@@ -19,9 +23,14 @@ $("#sonera").hover(function () {
 );
 
 $("#fetz").hover(function () {
+    $("#fetz").animate({
+        scale: scalefactor
+    }, scaledur );
     $("#sonera").fadeTo(fadedur, fadeopacity);
     $("#nicolelis").fadeTo(fadedur, fadeopacity);
 }, function () {
+    $("#fetz").stop(true, false);
+    $("#fetz").css("scale", 1);
     $("#sonera").stop(true, false);
     $("#sonera").css("opacity", 1);
     $("#nicolelis").stop(true, false);
@@ -30,9 +39,14 @@ $("#fetz").hover(function () {
 );
 
 $("#nicolelis").hover(function () {
+    $("#nicolelis").animate({
+        scale: scalefactor
+    }, scaledur );
     $("#sonera").fadeTo(fadedur, fadeopacity);
     $("#fetz").fadeTo(fadedur, fadeopacity);
 }, function () {
+    $("#nicolelis").stop(true, false);
+    $("#nicolelis").css("scale", 1);
     $("#sonera").stop(true, false);
     $("#sonera").css("opacity", 1);
     $("#fetz").stop(true, false);
