4eaf24f2c35d32863a50289ddf72a0c4db74245a
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-30 14:15:45 -0800

    fixing javascript

--- js/links.js
+++ js/links.js
@@ -1,13 +1,17 @@
-var elems = document.getElementsByClassName("email");
-console.log(elems.length);
-if (elems.length == 1){
-    elems.href = "mailto: richyjyun@gmail.com";
-    elems.target="_blank";
-    elems.rel="noopener noreferrer";
-}
-else{
-    for (i=0; i<elems.length; i++){
-        elems[i].href = "https://www.linkedin.com/in/richy-yun/";
-        console.log(elems[i].href);
+var email = document.getElementsByClassName("email");
+link(email);
+
+function link(elems){
+    if (elems.length == 1){
+        elems.href = "mailto: richyjyun@gmail.com";
+        elems.target="_blank";
+        elems.rel="noopener noreferrer";
+    }
+    else{
+        for (i=0; i<elems.length; i++){
+            elems[i].href = "mailto: richyjyun@gmail.com";
+            elems[i].target="_blank";
+            elems[i].rel="noopener noreferrer";
+        }
     }
 }
