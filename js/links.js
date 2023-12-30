ca691f0eb8641ed77501ea24cfbae7da28309928
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-30 14:11:47 -0800

    Change ref

--- js/links.js
+++ js/links.js
@@ -8,6 +8,6 @@ if (elems.length == 1){
 else{
     for (i=0; i<elems.length; i++){
         elems[i].href = "https://www.linkedin.com/in/richy-yun/";
+        console.log(elems[i].href);
     }
 }
-console.log(elems.href);
