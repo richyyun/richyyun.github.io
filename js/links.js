34e81cffbacbef38c6320d62af3b7e4025fc6710
Richy Yun <41391958+richyyun@users.noreply.github.com>
2023-12-30 14:20:32 -0800

    links to js

--- js/links.js
+++ js/links.js
@@ -1,15 +1,20 @@
-var email = document.getElementsByClassName("email");
-link(email);
+const classes = ["email","linkedin","github"];
+const urls = ["mailto: richyjyun@gmail.com", "https://www.linkedin.com/in/richy-yun/", "https://github.com/richyyun"];
 
-function link(elems){
+for (cl=0; cl<classes.length; cl++){
+    const elems = document.getElementsByClassName(classes[cl]);
+    link(elems, urls[cl]);
+}
+
+function link(elems, url){
     if (elems.length == 1){
-        elems.href = "mailto: richyjyun@gmail.com";
+        elems.href = url;
         elems.target="_blank";
         elems.rel="noopener noreferrer";
     }
     else{
         for (i=0; i<elems.length; i++){
-            elems[i].href = "mailto: richyjyun@gmail.com";
+            elems[i].href = url;
             elems[i].target="_blank";
             elems[i].rel="noopener noreferrer";
         }
