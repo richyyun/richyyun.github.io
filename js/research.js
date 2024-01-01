fbf5884ccebdc0afdb3a8ec22dfeb3171b4ffbc9
Richy Yun <41391958+richyyun@users.noreply.github.com>
2024-01-01 13:40:10 -0800

    Update research.js

--- js/research.js
+++ js/research.js
@@ -18,13 +18,15 @@ function showSlides(n) {
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        if (slides[i].style.display == "flex"){
+            slides[i].style.display = "none";
+        }
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "flex";
-    //slides[slideIndex-1].style.left = 0;
-    slides[slideIndex-1].style.transition = "1s";
+    slides[slideIndex-1].style.transition = "1s ease";
+    slides[slideIndex-1].style.left = "0%";
     dots[slideIndex-1].className += " active";
 }
