b852a4c64a0ef92eefea91b3e124f3a723c2efe4
Richy Yun <41391958+richyyun@users.noreply.github.com>
2024-01-04 19:55:25 -0800

    video fade in

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-$(".bg_vid").css('opacity', 0).animate( { opacity: 1 }, 'slow')
+var vid = document.getElementById("brainvid");
+vid.oncanplaythrough = function() {
+    setTimeout(function() {
+        var e = document.getElementById('brainvid');
+        fade(e);
+    }, 5000);
+};
+
+function fade(element) {
+    var op = 0;
+    var timer = setInterval(function() {
+        if (op >= 1) clearInterval(timer);
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1 || 0.1;
+    }, 50);
+}
