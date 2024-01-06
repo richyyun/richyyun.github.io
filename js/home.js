525ed74aae6846d45d95aefe2323db79825a40e3
Richy Yun <41391958+richyyun@users.noreply.github.com>
2024-01-05 21:54:10 -0800

    zindex nav

--- js/home.js
+++ js/home.js
@@ -2,24 +2,3 @@ var e = document.getElementById("brainvid");
 e.style.opacity = 0;
 
 $("#brainvid").fadeTo(2000, 1);
-
-/*
-var vid = document.getElementById("brainvid");
-
-vid.oncanplaythrough = function() {
-    setTimeout(function() {
-        var e = document.getElementById('brainvid');
-        fade(e);
-    }, 1000);
-};
-
-function fade(element) {
-    var op = 0;
-    var timer = setInterval(function() {
-        if (op >= 1) clearInterval(timer);
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1 || 0.1;
-    }, 100);
-}
-*/
